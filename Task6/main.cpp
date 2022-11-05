7feeac61af93cb90315019384fb7b1d8319fc52c
TMN <super.rikunen@gmail.com>
2022-11-05 09:10:44 +0300

    Initial Commit

--- Task6/main.cpp
+++ Task6/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(int argc, char **argv){
-    //expression example "int + {1,2,3} + {4,5,6}"
+    //expression example "int + {1,2,3} {4,5,6}"
     string expression = argv[1];
     vector<double> test_poly_koef;
     polynom test_poly(test_poly_koef);
